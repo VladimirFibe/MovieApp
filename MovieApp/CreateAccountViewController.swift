7c66d1e8d47c446a90615ea42c025dd93f91f94c
Pavel Gri <zlibabaika@gmail.com>
2023-04-04 01:40:03 +0400

    add start setting

---
+++ MovieApp/CreateAccountViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CreateAccountViewController.swift
+//  MovieApp
+//
+//  Created by Павел Грицков on 04.04.23.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor(named: "mainBlue")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
