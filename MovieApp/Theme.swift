57818589f0fa05e7eda119a4b0084f4ca7009184
Mark Raitman <markraitman@mail.ru>
2023-04-04 12:03:51 +0300

    App colors
    Main app colors added

---
+++ MovieApp/Theme.swift
@@ -0,0 +1,28 @@
+//
+//  Theme.swift
+//  MovieApp
+//
+//  Created by Марк Райтман on 04.04.2023.
+//
+
+import Foundation
+import UIKit
+
+enum Theme {
+    
+    //MARK: - Main colors
+    static let appColor: UIColor = UIColor(named: "appColor")!
+    static let blackColor: UIColor = .black
+    static let whiteColor: UIColor = .white
+    
+    //MARK: - Views' style
+#warning("здесь поправьте как нужно для вас:")
+    static let buttonCornerRadius: CGFloat = 15
+    static let imageCornerRadius: CGFloat = 20
+    
+    //MARK: - Layout
+#warning("здесь поправьте как нужно для вас:")
+    static let spacing: CGFloat = 20
+    static let leftOffset: CGFloat = 20
+    static let topOffset: CGFloat = 10
+}
