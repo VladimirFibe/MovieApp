fe2090b7a0a58cfc694387550736c9f0cd67ac7f
Vladimir <motiw@icloud.com>
2023-04-09 03:50:51 +0600

    add homeviewcontroller

---
+++ MovieApp/Feature/TabBar/Home/HomeViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+final class HomeViewController: BaseViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+}
+
