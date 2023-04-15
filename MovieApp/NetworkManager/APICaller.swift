f997f831a45ddd6c20620914dadd7378281108cc
Pavel Gri <zlibabaika@gmail.com>
2023-04-15 14:34:57 +0400

    added folder networkManager

---
+++ MovieApp/NetworkManager/APICaller.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+class APICaller{
+    
+    static let shared = APICaller()
+    private init(){}
+    
+    func fetchRequest<T: Decodable>(urlString: String, expecting: T.Type, completion: @escaping (T)-> Void) {
+        
+        let url = URL(string: urlString)
+        
+        URLSession.shared.dataTask(with: url!) { data, response, err in
+            if let err = err {
+                print(err)
+            }
+            
+            guard let data = data else { return }
+            
+            do {
+                let obj = try JSONDecoder().decode(expecting.self, from: data)
+                completion(obj)
+            } catch let jsonErr {
+                print("Failed \(jsonErr)")
+            }
+            
+        }.resume()
+    }
+    
+    func loadImage(stringURL: String, completion: @escaping (Data)->()){
+        
+        let url = URL(string: stringURL)
+        
+        DispatchQueue.global().async {
+            let data = try? Data(contentsOf: url!) //make sure your image in this url does exist, otherwise unwrap in a if let check / try-catch
+            completion(data!)
+        }
+    }
+}
