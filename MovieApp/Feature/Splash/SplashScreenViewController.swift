917e90e468fff05a65a1ada8cac1096f706f6735
Vladimir Fibe <motiw@icloud.com>
2023-08-28 11:28:41 +0600

    [wCBPHdcK] ready

--- MovieApp/Feature/Splash/SplashScreenViewController.swift
+++ MovieApp/Feature/Splash/SplashScreenViewController.swift
@@ -1,11 +1,4 @@
-
 import UIKit
-
-enum SplashScreen {
-    static let logotext = "Logo"
-    static let mainBlue = "mainBlue"
-    static let logoImage = "moviePlayLogo"
-}
 
 struct SplashScreenNavigation {
     let finish: Callback
@@ -13,6 +6,19 @@ struct SplashScreenNavigation {
 
 class SplashScreenViewController: BaseViewController {
     let navigation: SplashScreenNavigation
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = AppImage.splashLogo.uiImage
+        return imageView
+    }()
+    
+    private let loadingImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = AppImage.loading.uiImage
+        return imageView
+    }()
+    
     init(navigation: SplashScreenNavigation) {
         self.navigation = navigation
         super.init(nibName: nil, bundle: nil)
@@ -21,119 +27,32 @@ class SplashScreenViewController: BaseViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    let logoImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(named: "moviePlayLogo")
-        imageView.tintColor = .white
-        return imageView
-    }()
-    
-    let logoLabel: UILabel = {
-        let label = UILabel()
-        label.text = "MovieApp"
-        label.textAlignment = .center
-        label.textColor = .white
-        label.font = UIFont.systemFont(ofSize: 30, weight: .heavy)
-        return label
-    }()
-    
-    let customActivity: UIImageView = {
-        let image = UIImage(named: "LoadingPNG")
-        let imageView = UIImageView(image: image)
-        return imageView
-    }()
-    
-    var timer: Timer?
+}
 
-    // MARK: - Life cycle
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        configureUI()
-        setConstraints()
+extension SplashScreenViewController {
+    override func setupViews() {
+        view.backgroundColor = AppColor.mainPrimary.uiColor
+        [imageView, loadingImageView].forEach { view.addSubview($0)}
+        loadingAnimation()
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        startTimer()
-        
-        let delay: TimeInterval = 2
-        DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+    private func loadingAnimation() {
+        UIView.animate(withDuration: 3) {
+            self.loadingImageView.transform = CGAffineTransform.identity.rotated(by: CGFloat(Double.pi))
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
             self.navigation.finish()
         }
     }
     
-    // MARK: - Start setup
-    func configureUI() {
-        view.backgroundColor = UIColor(named: "mainBlue")
+    override func setupConstraints() {
+        imageView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
         
-        [logoImageView, logoLabel, customActivity].forEach { view in
-            self.view.addSubview(view)
-            view.translatesAutoresizingMaskIntoConstraints = false
+        loadingImageView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalToSuperview().inset(100)
         }
-    }
-    
-    // MARK: - Animation
-    @objc func animateCustomActivityIndicator() {
-        UIView.animate(withDuration: 0.8, delay: 0.0, options: .curveLinear) {
-            self.customActivity.transform = self.customActivity.transform.rotated(by: CGFloat(Double.pi))
-        } completion: { _ in
-            if self.timer != nil {
-                self.timer = Timer.scheduledTimer(
-                    timeInterval: 0.0, target: self,
-                    selector: #selector(self.animateCustomActivityIndicator),
-                    userInfo: nil, repeats: false)
-            }
-        }
-    }
-    
-    // MARK: - Timer methods
-    func startTimer() {
-        if timer == nil {
-            timer = Timer.scheduledTimer(
-                timeInterval: 0.0, target: self,
-                selector: #selector(animateCustomActivityIndicator),
-                userInfo: nil, repeats: false)
-        }
-    }
-    
-    func stopTimer() {
-        timer?.invalidate()
-        timer = nil
-    }
-    
-    // MARK: - Set constraints
-    func setConstraints() {
-        let center = NSLayoutConstraint(
-            item: logoImageView,
-            attribute: .centerY,
-            relatedBy: .equal,
-            toItem: view,
-            attribute: .centerY,
-            multiplier: 0.8,
-            constant: 0)
-        
-        NSLayoutConstraint.activate([
-            logoImageView.widthAnchor.constraint(equalToConstant: 88),
-            logoImageView.heightAnchor.constraint(equalTo: logoImageView.widthAnchor),
-            center,
-            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            
-            logoLabel.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 12),
-            logoLabel.heightAnchor.constraint(equalToConstant: 48),
-            logoLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            logoLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-
-            customActivity.widthAnchor.constraint(equalToConstant: 55),
-            customActivity.heightAnchor.constraint(equalTo: customActivity.widthAnchor),
-            customActivity.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            customActivity.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100),
-        ])
-    }
-    
-    deinit {
-        print("deinit")
     }
 }
